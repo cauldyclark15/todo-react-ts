14524da5b4ea9611ed0d7101f165d96920f72035
Joselie Castaneda <jcing.jc@gmail.com>
2020-08-14 07:29:38 +0800

    todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState, useRef } from "react";
+import "./App.css";
+
+interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
+interface Action {
+  id: string;
+  text: string;
+  type: "ADD_TODO" | "TOGGLE_TODO";
+}
+
+function todoReducer(state: Todo, action: Action): Todo {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false,
+      };
+    case "TOGGLE_TODO":
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return {
+        ...state,
+        completed: !state.completed,
+      };
+
+    default:
+      return state;
+  }
+}
+
+function reducer(state: Todo[], action: Action): Todo[] {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        todoReducer({ id: "1", text: "dummy todo", completed: false }, action),
+      ];
+    case "TOGGLE_TODO":
+      return state.map((item) => todoReducer(item, action));
+
+    default:
+      return state;
+  }
+}
+
+const initialState: Todo[] = [];
+let counter = 0;
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newText, setNewText] = useState("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        ref={inputRef}
+        value={newText}
+        name="newTodo"
+        onChange={(e) => setNewText(e.target.value)}
+        onKeyPress={(e) => {
+          if (e.key === "Enter" && newText) {
+            dispatch({
+              type: "ADD_TODO",
+              text: newText,
+              id: (counter++).toString(),
+            });
+
+            setNewText("");
+
+            const current = inputRef.current;
+
+            if (current && current.focus) {
+              current.focus();
+            }
+          }
+        }}
+        placeholder="Type here"
+      />
+
+      <button
+        type="button"
+        onClick={() => {
+          if (newText) {
+            dispatch({
+              type: "ADD_TODO",
+              text: newText,
+              id: (counter++).toString(),
+            });
+
+            setNewText("");
+
+            const current = inputRef.current;
+
+            if (current && current.focus) {
+              current.focus();
+            }
+          }
+        }}
+      >
+        Save
+      </button>
+
+      <ol>
+        {state.map((item) => (
+          <li
+            style={{
+              cursor: "pointer",
+              textDecoration: item.completed ? "line-through" : "none",
+              padding: 10,
+            }}
+            onClick={() =>
+              dispatch({ type: "TOGGLE_TODO", id: item.id, text: "dummy text" })
+            }
+            key={item.id}
+          >
+            {item.text}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
